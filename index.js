c4df30a0cc409ab5acf870ae2c7c360ea303129d
Lionel Landwerlin <llandwerlin@gmail.com>
2014-03-26 13:03:05 +0000

    fix component name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * This file can be used for general library features of noflo-gpio.
+ * This file can be used for general library features of noflo-tween.
  *
  * The library features can be made available as CommonJS modules that the
  * components in this project utilize.
